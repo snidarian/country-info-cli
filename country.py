f4f5476798ca452e729e80b29f0c91da99471072
snidarian <cephalopod31956@gmail.com>
2021-06-17 11:07:33 -0400

    adding args

--- country.py
+++ country.py
@@ -3,6 +3,7 @@
 
 import requests as rq
 import random as r
+import argparse
 from colorama import Fore, Style
 
 
@@ -13,14 +14,26 @@ reset = Fore.RESET
 
 
 
+# setup argparse
+parser = argparse.ArgumentParser(description="Query country info")
+
+args = parser.add_argument('--all', '-a', help="Shows all countries and their calling codes")
+args = parser.add_argument('fullname', help="Shows all countries and their calling codes", type=str, nargs='?')
+
+args = parser.parse_args()
+
+
+# makes request to webAPI for country information
 result = rq.get('https://restcountries.eu/rest/v2/all')
 
 
 json_payload_list = result.json()
 
+
+
 index = 0
 for country_item in json_payload_list:
-    print(str(index) + '. ' + country_item['name'] + ' - ' + country_item['alpha3Code'])
+    print(str(index) + '. ' + country_item['name'] + ' - ' + country_item['callingCodes'][0])
     index+=1
 
 
